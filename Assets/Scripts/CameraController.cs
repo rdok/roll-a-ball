600dd882fd0b28c2e71f3c8e48d3c1791ebd5bde
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-13 00:36:57 +0200

    Attach camera to player.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        public GameObject Player;
+        private Vector3 _offest;
+
+        public void Start ()
+        {
+            _offest = transform.position - Player.transform.position;
+        }
+	
+        public void LateUpdate ()
+        {
+            transform.position = Player.transform.position + _offest;
+        }
+    }
+}
