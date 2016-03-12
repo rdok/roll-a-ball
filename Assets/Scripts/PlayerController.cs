981286a78361e507124501dc43d676f98075b670
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-12 10:58:28 +0200

    Set player default speed. Resolve #1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,23 @@ namespace Assets.Scripts
 {
     public class PlayerController : MonoBehaviour
     {
+        public float Speed = 10;
+
         private Rigidbody _rigidbody;
 
-        void Start()
+        public void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
         }
 
-        void FixedUpdate()
+        public void FixedUpdate()
         {
-            // check every frame for player input
-            // foreach input apply that input to every frame as movement
-
-
             var moveHorizontal = Input.GetAxis("Horizontal");
             var moveVertical = Input.GetAxis("Vertical");
 
             var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-            _rigidbody.AddForce(movement);
+            _rigidbody.AddForce(movement * Speed);
         }
     }
 }
