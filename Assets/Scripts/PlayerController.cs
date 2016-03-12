8e191de40bc8ed25304677f10f317270785a8fe8
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-12 10:46:13 +0200

    Add movement to player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            // check every frame for player input
+            // foreach input apply that input to every frame as movement
+
+
+            var moveHorizontal = Input.GetAxis("Horizontal");
+            var moveVertical = Input.GetAxis("Vertical");
+
+            var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+            _rigidbody.AddForce(movement);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+#### Goals
+##### Player Behavior
+- Roll around the game aread
+- Bump into walls
+- Stay on the ground, not fly into space
+- Collide with collectible game objects
+- Pick collectible game objects on collision
+#### Set up
+- Open MiniGame scene (File -> Open Scene -> _Scenes -> MiniGame.unity)
