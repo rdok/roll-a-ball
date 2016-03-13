cc2db6e47a582ff26fff3ba8c3eca3cf535e83b3
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-13 13:03:02 +0200

    Add collecatable items, and rotate them.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Rotator : MonoBehaviour
+    {
+        public void Update()
+        {
+            var randomRotation = new Vector3(Random.Range(5.0F, 10.0F), Random.Range(15.0F, 30.0F),
+                Random.Range(20.0F, 40.0F));
+
+            transform.Rotate(randomRotation*Time.deltaTime);
+        }
+    }
+}
